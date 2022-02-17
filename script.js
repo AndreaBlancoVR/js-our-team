43f0e8be5ad1f20c171fc3162e36083575da6f85
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-02-17 20:20:01 +0100

    Esercizio completato

--- script.js
+++ script.js
@@ -51,18 +51,38 @@ console.log(TEAM_CONTAINER)
 
 
 
-const TEAM_CARD = `<div class="team-card">
+// const TEAM_CARD = `<div class="team-card">
+// <div class="card-image">
+//   <img
+//     src="img/${team[0].image}"
+//     alt="${team[0].name}"
+//   />
+// </div>
+// <div class="card-text">
+//   <h3>${team[0].name}</h3>
+//   <p>${team[0].role}</p>
+// </div>
+// </div>`
+
+// TEAM_CONTAINER.innerHTML += TEAM_CARD
+
+
+// creo un ciclo che legga il contenuto dell'array e 
+// lo uso per assegnare le caratterisiche degli elementi 
+// creati all'interno del ciclo stesso
+for( let i = 0; i < team.length; i++) {
+    let membro = team[i];
+    membro = `<div class="team-card">
 <div class="card-image">
   <img
-    src="img/${team[0].image}"
-    alt="${team[0].name}"
+    src="img/${team[i].image}"
+    alt="${team[i].name}"
   />
 </div>
 <div class="card-text">
-  <h3>${team[0].name}</h3>
-  <p>${team[0].role}</p>
+  <h3>${team[i].name}</h3>
+  <p>${team[i].role}</p>
 </div>
 </div>`
-
-TEAM_CONTAINER.innerHTML += TEAM_CARD
-
+TEAM_CONTAINER.innerHTML += membro
+}
