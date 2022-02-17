497f72a4ed276df760864fcec14ccadc6b1ad852
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-02-17 19:06:12 +0100

    creato prototipo di card

---
+++ script.js
@@ -0,0 +1,51 @@
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+const TEAM_CONTAINER = document.querySelector('.team-container')
+console.log(TEAM_CONTAINER)
+const TEAM_CARD = `<div class="team-card">
+<div class="card-image">
+  <img
+    src="img/wayne-barnett-founder-ceo.jpg"
+    alt="Wayne Barnett"
+  />
+</div>
+<div class="card-text">
+  <h3>Wayne Barnett</h3>
+  <p>Founder & CEO</p>
+</div>
+</div>`
+
+TEAM_CONTAINER.innerHTML += TEAM_CARD
+
+console.log(TEAM_CONTAINER)
